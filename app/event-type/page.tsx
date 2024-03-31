40498034b9ff2bf01bd909597de15dcff0eb9a91
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-03-31 19:27:53 +0545

    chore: change font and primary and secondary color

---
+++ app/event-type/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+const postList = [
+  {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  },
+];
+const EventTypePage = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {};
+  return (
+    <div>
+      <h1 className="font-bold text-3xl mb-5">Event Type example</h1>
+      {/* for filter */}
+      <form onSubmit={handleSubmit}>
+        <input className="border rounded-md p-2" type="text" name="search" />
+        <Button
+          type="submit"
+          className="m-1 bg-neutral-500 text-white"
+          variant="outline"
+        >
+          Search
+        </Button>
+      </form>
+      {/* for displaying  posts */}
+      <ul className="list-group grid gap-4 my-4">
+        {postList.map((item) => (
+          <li key={item.id}>
+            <h2 className="mb-2 text-2xl font-bold">{item.title}</h2>
+            <p className="text-gray-500">{item.body}</p>
+            <Button variant="outline" className="bg-red-500 text-white">
+              Delete
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default EventTypePage;
