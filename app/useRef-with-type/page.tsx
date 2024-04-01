6d233efdebd7f59aa90897803fc3a0784af49db8
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-04-01 23:21:06 +0545

    chore: work with useref type

---
+++ app/useRef-with-type/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Section from "@/components/Section";
+import { Button } from "@/components/ui/button";
+import React, { useEffect, useRef } from "react";
+
+const UseRefWithType = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const usernameInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    //focusing the input
+    inputRef.current?.focus();
+  }, []);
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold">Use Ref with Type</h1>
+      <Section />
+      <div className="grid justify-center">
+        <form action="" className="grid gap-4 w-[300px]">
+          <input
+            type="text"
+            placeholder="focus here"
+            name="focusInput"
+            className="p-2"
+            ref={inputRef}
+          />
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            className="p-2"
+            ref={usernameInputRef}
+          />
+          <Button>Send</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UseRefWithType;
