e55c574048928a7b5ab59240278ad1be89e934e4
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-08-15 15:29:26 +0545

    introduction of optional chaining while dealing with optional properties

--- app/optional-property/page.ts
+++ app/optional-property/page.ts
@@ -1,14 +1,14 @@
-type ObjTypes = {
+type ObjType = {
   name: string;
   city?: string;
 };
 
-const greets = (obj: { name: string; city?: string }) => {
+const greet = (obj: ObjType) => {
   console.log(obj.city);
   // when we apply method in optional properties, we need to check if it is undefined
   console.log(obj.city?.toUpperCase());
 };
 
-const obj1s = { name: "abhishek" };
+const obj1 = { name: "abhishek" };
 
-greet(obj1s);
+greet(obj1);
