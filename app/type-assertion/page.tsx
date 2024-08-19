90b56d198d6aadb5955b54b8ac7697fd69f27707
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-08-19 15:19:49 +0545

    chore: type assertion and null checking

--- app/type-assertion/page.tsx
+++ app/type-assertion/page.tsx
@@ -1,9 +1,20 @@
-// type assertion
-const myCanvas1 = document.getElementById("main_canvas") as HTMLCanvasElement;
+import React from "react";
+// type assertions are generally used in dom manipulation and async operation like data fetching.
+// But even if you wont use type assertion, TS will automatically handle it.
+// TS also checks for null itself automatically by issuing an error in editor
+const TypeAssertionPage = () => {
+  // type assertion
+  const myCanvas1 = document.getElementById(
+    "main_canvas"
+  ) as HTMLElement | null;
+  console.log(myCanvas1?.ATTRIBUTE_NODE);
 
-// In a .tsx file, there might be conflicts when using angle brackets for type assertions if the TypeScript compiler confuses it with JSX. To avoid this, you can use the as syntax for type assertions like above
-// While in ts file, you can still use angle bracket
+  // In a .tsx file, there might be conflicts when using angle brackets for type assertions if the TypeScript compiler confuses it with JSX. To avoid this, you can use the as syntax for type assertions like above.
+  // While in ts file, you can still use angle bracket like below:
 
-// const myCanvas2 = <HTMLCanvasElement>document.getElementById("main_canvas");
+  // const myCanvas2 = <HTMLCanvasElement>document.getElementById("main_canvas");
 
-const x = "hello" as any as number;
+  return <div id="main_canvas"></div>;
+};
+
+export default TypeAssertionPage;
