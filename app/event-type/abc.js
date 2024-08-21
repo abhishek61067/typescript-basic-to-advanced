0a8757c71faabcea08bbb4fc5f7682a7f76d3f34
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-08-21 19:13:26 +0545

    chore: literal inference

--- app/event-type/abc.js
+++
@@ -1,6 +0,0 @@
-function greet(person, date) {
-  console.log(
-    "Hello ".concat(person, ", today is ").concat(date.toDateString(), "!")
-  );
-}
-greet("Maddison", new Date());
---
+++ app/event-type/abc.ts
@@ -0,0 +1,4 @@
+declare function handleRequest(url: string, method: "GET" | "POST"): void;
+
+// const req = { url: "https://example.com", method: "GET" };
+handleRequest("https://example.com", "GET");
