2c3d7bba57726f058861d6d54d38854528ce6088
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-09-04 17:38:37 +0545

    generic for class

--- app/generics/page.tsx
+++ app/generics/page.tsx
@@ -8,6 +8,28 @@ function createPair<s, t>(v1: s, v2: t): [s, t] {
 }
 console.log(createPair<string, number>("hello", 42)); // ['hello', 42]
 
+class NamedValue<T> {
+  private _value: T | undefined;
+
+  constructor(private name: string) {}
+
+  public setValue(value: T) {
+    this._value = value;
+  }
+
+  public getValue(): T | undefined {
+    return this._value;
+  }
+
+  public toString(): string {
+    return `${this.name}: ${this._value}`;
+  }
+}
+
+let value = new NamedValue<number>("myNumber");
+value.setValue(10);
+console.log(value.toString()); // myNumber: 10
+
 const ItemList = [
   {
     userId: 1,
