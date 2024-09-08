68f29d4f08e16170f1b31b906c4635d23e649388
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-09-08 12:51:31 +0545

    chore: readonly utility type

--- types/types.tsx
+++ types/types.tsx
@@ -10,3 +10,11 @@ export type ObjType2 = {
   maritalStatus: boolean;
   sex?: string;
 };
+
+export type Type3 = string | number | boolean;
+
+export type funcType1 = () => { x: number };
+
+export type funcType2 = (p1: { x: number }, p2: { y: number }) => void;
+
+export type ObjType3 = { name: string };
